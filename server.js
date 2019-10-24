45fdd2e3abf94de813d3cae589c98fd716330488
pablo <raselberg@gmail.com>
2019-10-23 19:22:38 -0700

    updateroutes

--- server.js
+++ server.js
@@ -84,26 +84,73 @@ app.get("/scrape", function (req, res) {
 
 // Route for getting all Articles from the db
 app.get("/getInfo", function (req, res) {
-  // TODO: Finish the route so it grabs all of the articles
+    // Grab every document in the Articles collection
+    db.Post.find({})
+    .then(function(bptPost) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(bptPost);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
-  // TODO
-  // ====
-  // Finish the route so it finds one article using the req.params.id,
-  // and run the populate method with "note",
-  // then responds with the article with the note included
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Post.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
+
+//this route is not ready for the user to onclick the save btn, this will add "saved:true" to mongo db
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function (req, res) {
-  // TODO
-  // ====
-  // save the new note that gets posted to the Notes collection
-  // then find an article from the req.params.id
-  // and update it's "note" property with the _id of the new note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Post.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Post.findOneAndUpdate({ _id: req.params.id }, { save: true });
+    })
+    .then(function(Post) {
+      // If we were able to successfully update an Post, send it back to the client
+      res.json(Post);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
+
+//when we show the saved tab, go to this routing "/saved"
+//then it will make comm. to mongo db and look for all data with "save:true" 
+//and only show articles with save:true
+app.get("/saved", function(req, res) {
+  // Go into the mongo collection, and find all docs where "read" is false
+  db.Post.find({ save: true }, function(error, found) {
+    // Show any errors
+    if (error) {
+      console.log(error);
+    }
+    else {
+      // Otherwise, send the books we found to the browser as a json
+      res.json(found);
+    }
+  });
+});
+
 
 // Connect to the Mongo DB
 var CONNECTION_URI = process.env.MONGODB_URI || "mongodb://localhost/BPT";
