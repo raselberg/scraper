a3374f2a007e9bc684677d96249030ea03cd1510
pablo <raselberg@gmail.com>
2019-10-24 18:14:25 -0700

    finalCommitScrape

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$.getJSON("/getInfo", function(data) {
+  console.log(data)
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#Post").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
   }
 });
 
